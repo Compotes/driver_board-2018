d7ac3fd2dc280e4f7dfbd75100b96fde50adb4a3
dawd <dwadaw@dwa.com>
2017-04-12 14:59:04 +0200

    Motor thread added.
    Signed-off-by: dawd <dwadaw@dwa.com>

--- programs/driver_board/main.c
+++ programs/driver_board/main.c
@@ -6,16 +6,18 @@
 #include <inttypes.h>
 
 #include "communication.h"
+#include "motor.h"
 
-#define MOTOR_PWM_CHANNEL 3
+#define MISTAKE 0
 
 #define UNUSED(arg) (void)(arg)
 
 thread_t *commanderp;
-
-int16_t motor_speed = 0;
+thread_t *motorp;
 
 void execute_master_command(uint16_t command_id, uint8_t *buff) {
+    uint8_t j;
+    int16_t motor_speed = 0;
     switch (command_id) {
         case 0xAA:  
             led(GREEN, buff[0]);
@@ -23,8 +25,16 @@ void execute_master_command(uint16_t command_id, uint8_t *buff) {
 
         case 0xBB:
             // Motor movement command
-            motor_speed = (buff[1] * 1000 + buff[2] * 100 + buff[3] * 10 + buff[4]);
+            motor_speed = 0;
+            motor_speed |= buff[1];
+            motor_speed |= buff[2] << 4;
+            motor_speed |= buff[3] << 8;
+
+            if(motor_speed != 0) motor_speed += MISTAKE;
+
             if(buff[0] == 1) motor_speed *= -1;
+
+            chMsgSend(motorp, motor_speed);
 
         default:    
             break;
@@ -40,40 +50,6 @@ uint8_t *send_data_command(uint16_t command_id, uint8_t data_length, uint8_t *bu
             break;           
     }
     return buff;
-}
-
-void motor_forward(void) {
-    palSetPad(MOTOR_IN_GPIO, MOTOR_IN_1);
-    palClearPad(MOTOR_IN_GPIO, MOTOR_IN_2);
-}
-
-void motor_backward(void) {
-    palSetPad(MOTOR_IN_GPIO, MOTOR_IN_2);
-    palClearPad(MOTOR_IN_GPIO, MOTOR_IN_1);
-}
-
-void motor_stop(void) {
-    palClearPad(MOTOR_IN_GPIO, MOTOR_IN_1);
-    palClearPad(MOTOR_IN_GPIO, MOTOR_IN_2);
-}
-
-THD_WORKING_AREA(waMotorThread, 64);
-THD_FUNCTION(MotorThread, arg) {
-    UNUSED(arg);
-    while (1) { 
-        if (motor_speed < 0) {
-            motor_backward();
-            pwmEnableChannel(&PWMD3, MOTOR_PWM_CHANNEL, -motor_speed);
-        } else if(motor_speed > 0) {
-            motor_forward();
-            pwmEnableChannel(&PWMD3, MOTOR_PWM_CHANNEL, motor_speed);
-        } else {
-            motor_stop();
-        }
-        chThdSleepMilliseconds(100);
-    }
-    chRegSetThreadName("motor");
-    chThdSleepMilliseconds(100);
 }
 
 THD_WORKING_AREA(waCommanderThread, 128);
@@ -114,21 +90,6 @@ THD_FUNCTION(CommanderThread, arg) {
     chThdSleepMilliseconds(100);
 }
 
-static PWMConfig pwmcfg = {
-    22000, // 22kHz frequency
-    1000, // 1000 ticks - period lenght
-    NULL,
-    {
-        {PWM_OUTPUT_DISABLED, NULL},
-        {PWM_OUTPUT_DISABLED, NULL},
-        {PWM_OUTPUT_DISABLED, NULL},
-        {PWM_OUTPUT_ACTIVE_HIGH, NULL}
-    },
-    0,
-    0
-};
-
-
 int main(void) {    
     halInit();
     chSysInit();
@@ -138,30 +99,17 @@ int main(void) {
      */
 
     communication_init();
-
-    /*
-     * PWM setup
-     */  
-
-    pwmInit();
-    pwmStart(&PWMD3, &pwmcfg);
-
-    palSetPadMode(MOTOR_IN_GPIO, MOTOR_IN_1, PAL_MODE_OUTPUT_PUSHPULL);
-    palSetPadMode(MOTOR_IN_GPIO, MOTOR_IN_2, PAL_MODE_OUTPUT_PUSHPULL);
-    
-    palSetPadMode(MOTOR_ENABLE_GPIO, MOTOR_ENABLE, PAL_MODE_ALTERNATE(1));
     
     /*
      * Threads setup
      */
 
-    chThdCreateStatic(waMotorThread, sizeof(waMotorThread),
-                      NORMALPRIO, MotorThread, NULL);
-
     commanderp = chThdCreateStatic(waCommanderThread, sizeof(waCommanderThread),
                                    NORMALPRIO, CommanderThread, NULL);
 
     communication_thread(commanderp);
+
+    motorp = motor_thread_init();
 
     while (1) {
         chThdSleepMilliseconds(100);
